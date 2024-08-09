0129924ddad2fde701103a38e1f7d0bb60df64ca
P3REZ05 <dpanimalito@gmail.com>
2024-08-09 18:00:18 -0500

    cambios 3

--- src/database.py
+++ src/database.py
@@ -7,7 +7,7 @@ db = mysql.connector.connect(
     user=os.getenv("DB_USER", "perez"),
     password=os.getenv("DB_PASSWORD", ""),
     database=os.getenv("DB_NAME", "mercaflask"),
-    port=os.getenv("DB_PORT", "3306")
+    port=os.getenv("DB_PORT", 3306)
 )
 
 def get_db_cursor():
@@ -19,5 +19,5 @@ database = mysql.connector.connect(
     user=os.getenv("DB_USER", "perez"),
     password=os.getenv("DB_PASSWORD", ""),
     database=os.getenv("DB_NAME", "mercaflask"),
-    port=os.getenv("DB_PORT", "3306")
+    port=os.getenv("DB_PORT", 3306)
 )
